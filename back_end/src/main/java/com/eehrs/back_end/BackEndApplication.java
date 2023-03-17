826155fb80750333998952965fba279fb11b4316
Betel Ameha <betel.ameha@gmail.com>
2023-03-17 12:53:31 +0300

    spring start up project

--- back_end/src/main/java/com/eehrs/back_end/BackEndApplication.java
+++ back_end/src/main/java/com/eehrs/back_end/BackEndApplication.java
@@ -1,4 +1,4 @@
-package com.eehrs.backend;
+package com.eehrs.back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- back_end/src/test/java/com/eehrs/back_end/BackEndApplicationTests.java
+++ back_end/src/test/java/com/eehrs/back_end/BackEndApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eehrs.backend;
+package com.eehrs.back_end;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
