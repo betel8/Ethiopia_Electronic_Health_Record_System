df7c35c1d0547e9a576912cc60caa52d016c3331
Betel Ameha <betel.ameha@gmail.com>
2023-04-21 00:58:06 +0300

    saving

--- front_end/src/ActivityMonitor/ActivityContainer.js
+++ front_end/src/ActivityMonitor/ActivityContainer.js
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
-import './activity.css'
+import './activity.css';
 import SingleActivity from "./SingleActivity";
-
-import { FaRedoAlt } from 'react-icons/fa'
+import { FaRedoAlt } from 'react-icons/fa';
 function ActivityContainer(props) {
     const lists = { name: "tg", status: "New Doctor Added" };
     return (
         <div className="activityContainer">
-            <div className="sub-header">
 
+            <div className="sub-header">
                 <h4>Employee Activity Monitor</h4>
-                <div className="refreshBox">
+                {/*<div className="refreshBox">
                 <FaRedoAlt  size={"0.8rem"} />
                 <span style={{fontSize:"small"}}> Refresh</span>
-                </div>
+    </div>*/}
             </div>
-            <div style={{display:"flex",position:"relative",padding:"0rem 1rem"}}>
-                <span style={{position:"relative"}}>Name</span>
-                <span style={{position:"relative",marginLeft:"5rem"}}>Email</span>
-                <span style={{position:"relative",marginLeft:"5rem"}}>Phone</span>
-                <span style={{position:"relative",marginLeft:"5rem"}}>Status</span>
-                <span style={{position:"relative",marginLeft:"5rem"}}>Last Activity</span>
-            </div>
+            <ul style={{display:"flex",position:"relative",padding:"0rem ",margin:0}}>
+                <li style={{position:"relative",width:"10vw"}}>Name</li>
+                <li style={{position:"relative",width:"15vw"}}>Email</li>
+                <li style={{position:"relative",width:"10vw"}}>Phone</li>
+                <li style={{position:"relative",width:"4vw"}}>Status</li>
+                <li style={{position:"relative",width:"15vw"}}>Last Activity</li>
+            </ul>
   
             <div className="activityBox" id="scroll-style">
-                <SingleActivity/>
+                <SingleActivity name={"betel ameha"} email={"betel.ameha@gmail.com"} phone={"0911448314"} status={true} 
+                activity={"checking"} activityTitle={"home"} />
             </div>
         </div>
     );
--- front_end/src/loading/loading.js
+++ front_end/src/loading/loading.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import './loading.css'
+import "./Loading.css"
 import image from '../image/loading.gif'
 function Loading(){
     return(
-<div >
-    <img className='img' src={image} alt=""/>
-</div>
+        <div className='loading' >
+            <img className='img' src={image} alt=""/>
+        </div>
     );
 }
 export default Loading
