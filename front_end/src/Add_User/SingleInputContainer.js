1112cceef17b8ffcfc995222dcdc27b2e4354e40
Tigist <tgyegezu@gmail.com>
2023-04-21 09:14:26 -1100

    Patient Record

--- front_end/src/Add_User/SingleInputContainer.js
+++ front_end/src/Add_User/SingleInputContainer.js
@@ -20,7 +20,19 @@ function SingleInputContainer(props){
         }
         
     }
-if(props.type!=="select"){
+    if(props.type==="textarea"){
+        return(
+         
+        <div className='singleContainer' >
+         
+            { (props.label==='History:') ?  <textarea cols="40" rows="8" onChange={onChangeHandler} name={props.name} ></textarea>
+          
+            :<textarea cols="40" rows="3" onChange={onChangeHandler} name={props.name}></textarea>
+        }
+          <label htmlFor={props.name}className={labelFocus}>{props.label}</label>
+   
+      </div> );}
+        else if(props.type!=="select"){
     return(
         <div className='singleContainer'>
         <input type={props.type} className= {props.name+"Input"}  name={props.name} 
@@ -28,16 +40,45 @@ if(props.type!=="select"){
         <label htmlFor={props.name}className={labelFocus}>{props.label}</label>
         {props.name==='email'&&<FaEnvelope style={{position:'absolute',top:"3.5vh",right:"1vw"}}/>}
         {props.error && (<p className="warning"><FaExclamationTriangle/>{props.label+props.error}</p>)}
+      
         </div>
     );
-}else{
+}
+else{
     return(
         <div className='singleContainer'>
+           {  (props.label==="Gender:" )?
+            
         <select onChange={onChangeHandler} name={props.name}>
             <option></option>
             <option>Male</option>
             <option>Female</option>
-        </select>
+        </select >
+         :(props.label=="Peripheries:")?
+         <select  onChange={onChangeHandler} name={props.name}>
+         <option></option>
+         <option>Cold</option>
+         
+         <option>Warm</option>
+         <option>Cynosed</option>
+     </select>
+      :(props.label=="GCS:")?
+      <select  onChange={onChangeHandler} name={props.name}>
+      <option></option>
+      <option>E</option>
+      
+      <option>M</option>
+      <option>V</option>
+  </select>
+        :
+         <select  id="LargeSelect" onChange={onChangeHandler} name={props.name}>
+         <option></option>
+         <option>Moles</option>
+         
+         <option>naevus</option>
+         <option>Defomities</option>
+     </select>
+        }
         <label htmlFor={props.name}className={labelFocus}>{props.label}</label>
         {props.error && (<p className="warning"><FaExclamationTriangle/>{props.label+props.error}</p>)}
         </div>
--- front_end/src/patientRecord/patientRecord.js
+++ front_end/src/patientRecord/patientRecord.js
@@ -11,6 +11,7 @@ function PatientRecord(){
            'validationType':'name',
             'label':'Name:'   
         },
+        
         {
           'name':'Age',
           'type':'number',
@@ -25,36 +26,130 @@ function PatientRecord(){
         },
         {
           'name':'Dadmission',
-          'type':'Date/time',
-          'validationType':'Dadmission',
+          'type':'datetime-local',
+          'validationType':"name",
           'label':'Date/Time of Admission:'
+        },
+        {
+          'name':'PhoneNo',
+          'type':'tel',
+          'validationType':'phone',
+          'label':'Phone No:'
         },
         {
           'name':'Nstaff',
           'type':'text',
-          'validationType':'staffname',
+          'validationType':"name",
           'label':'Patient received by(Name of Staff Nurse):'
         },
-        {
-          'name':'PhoneNo',
-          'type':'tel',
-          'validationType':'cellphone',
-          'label':'Phone No:'
-        },
+     
         {
           'name':'ReasonICU',
-          'type':'text',
+          'type':'textarea',
           'validationType':'reason',
           'label':'Reason for transfer to ICU'
         },
         {
-          'name':'PhoneNo',
-          'type':'tel',
-          'validationType':'cellphone',
-          'label':'Phone No:'
+          'name':'regular',
+          'type':'textarea',
+          'label':'Regular medication on:',
+          
+        'validationType':"name",
         },
-        
+        {
+          'name':'allergy',
+          'type':'textarea',
+          'validationType':'name',
+          'label':'Allergies Known(if any):'
+        },
+        {
+          'name':'Pre',
+          'type':'textarea',
+          'validationType':'name',
+          'label':'Pre Existing Conditions:'
+        },
+        {
+          'name':'complaints',
+          'type':'textarea',
+          'validationType':'name',
+          'label':'Presenting complaints:'
+        },
+        {
+          'name':'history',
+          'type':'textarea',
+          'validationType':'name',
+          'label':'History:'
+        },
+        {
+          'name':'bp',
+          'type':'text',
+          'validationType':'name',
+          'label':'BP:'
+        },
+        {
+          'name':'pulse',
+          'type':'text',
+          'validationType':'name',
+          'label':'Pulse:'
+        },
+        {
+          'name':'resp',
+          'type':'text',
+          'validationType':'name',
+          'label':'Resp:'
+        },
+        {
+          'name':'temprature',
+          'type':'text',
+          'validationType':'name',
+          'label':'Temprature:'
+        },
+        {
+          'name':'gcs',
+          'type':'select',
+          'validationType':'name',
+          'label':'GCS:'
+        },
+        {
+          'name':'o2',
+          'type':'text',
+          'validationType':'name',
+          'label':'O2 Sat:'
+        },
+        {
+          'name':'pupils',
+          'type':'text',
+          'validationType':'name',
+          'label':'Pupils:'
+        },
+        {
+          'name':'peripheries',
+          'type':'select',
+          'validationType':'name',
+          'label':'Peripheries:'
+        },
+        {
+          'name':'mark',
+          'type':'select',
+          'validationType':'name',
+          'label':'Identification Mark if any:'
+        },
+        {
+          'name':'skin',
+          'type':'textarea',
+          'validationType':'name',
+          'label':'Skin Status:'
+        },
+        {
+          'name':'surgical',
+          'type':'textarea',
+          'validationType':'name',
+          'label':'Sergical scars if any:'
+        },
     ]
+    
+    
+
   const {integratedValue,handler,handleSubmit}=Warning(PatientRecordInput,"patient");  
     
     function add (){
@@ -66,13 +161,61 @@ function PatientRecord(){
         label={value.label} error={value.error} required={value.required} validationType={value.validationType} />);
       })
     return(
+      <section className='add_User' >
+      <div className='add_user_Title'>
+        <h2>Add New Patient</h2>
+    </div>
         <form onSubmit={handleSubmit}>
            <div className='formInput'>
-         
-              {PatientRecordInputValues[0]}
+           <div className='basic_Information'> 
+           <h3 className="title">Patient registration</h3>
+                  <div className='phoneNumber'>
+              
+            {PatientRecordInputValues[0]}{PatientRecordInputValues[1]}{PatientRecordInputValues[2]}
             </div>
-       
+            
+            <div className='name'>
+            {PatientRecordInputValues[3]}
+            
+               {PatientRecordInputValues[4]}
+               
+               {PatientRecordInputValues[5]}
+               {PatientRecordInputValues[6]}
+              
+              {PatientRecordInputValues[7]}
+              {PatientRecordInputValues[8]}
+              {PatientRecordInputValues[9]}
+              
+              {PatientRecordInputValues[10]}
+              </div>
+            </div>
+          <hr className='middleHr'/>
+             
+           
+            <div className="additional_information">
+            <h3 className="title">Previous History</h3>
+             {PatientRecordInputValues[11]}
+             <hr className="middleHr"/>
+              <h3 className="title">General Examination On Admission</h3>
+             <div style={{display:"flex"}}>
+              {PatientRecordInputValues[12]}{PatientRecordInputValues[13]}
+              </div>
+              <div style={{display:"flex"}}>
+                {PatientRecordInputValues[14]}{PatientRecordInputValues[15]}{PatientRecordInputValues[16]}
+              </div>
+              <div style={{display:"flex"}}>
+                {PatientRecordInputValues[17]}{PatientRecordInputValues[18]}
+              </div>
+              {PatientRecordInputValues[19]}  
+              {PatientRecordInputValues[20]}
+             
+              {PatientRecordInputValues[21]}
+              {PatientRecordInputValues[22]}
+          
+            
+       </div>
+       </div>
            <button type="submit">Submit</button>
-        </form>
+        </form></section>  
 );}
 export default PatientRecord
