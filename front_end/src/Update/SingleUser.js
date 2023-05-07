5bed3bbbb524f26503a8277f629459b923d8faba
Tigist <tgyegezu@gmail.com>
2023-05-07 09:00:50 -1100

    new

--- front_end/src/Update/SingleUser.js
+++ front_end/src/Update/SingleUser.js
@@ -3,16 +3,6 @@ import { AiFillEdit } from "react-icons/ai";
 function SingleUser(props){
     return(
         <div>
-<ul style={{display:'flex'}}>
-    <li><RiUser2Fill style={{width:'10vw',height:'10vh'}}/></li>
-    <li><input value='Full Name'/><AiFillEdit/></li>
-</ul>
-<ul style={{display:'flex'}}>
-    <li><input value='Status'/><AiFillEdit/></li>
-    <li><input value='Cell Phone'/><AiFillEdit/></li>
-    <li><input value='Email'/><AiFillEdit/></li>
-
-</ul>
         </div>
         );
     }
