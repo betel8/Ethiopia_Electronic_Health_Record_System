d6704e5c6094410a0791fa4a07968c3cf585ebd4
Tigist <tgyegezu@gmail.com>
2023-05-07 09:06:15 -1100

    new

--- front_end/src/Update/SingleUser.js
+++ front_end/src/Update/SingleUser.js
@@ -1,8 +1,24 @@
 import { RiUser2Fill } from "react-icons/ri";
 import { AiFillEdit } from "react-icons/ai";
+
+import SingleInputContainer from '../SharedComponents/SingleInputContainer';
+import Warning from "../SharedComponents/warning";
+import CONSTANT from "../Constant";
+
 function SingleUser(props){
     return(
         <div>
+            <ul style={{display:'flex'}}>
+    <li><RiUser2Fill style={{width:'10vw',height:'10vh'}}/></li>
+    <li><input value='Full Name'/><AiFillEdit/></li>
+</ul>
+<ul style={{display:'flex'}}>
+    <li><input value='Status'/><AiFillEdit/></li>
+    <li><input value='Cell Phone'/><AiFillEdit/></li>
+    <li><input value='Email'/><AiFillEdit/></li>
+
+</ul>
+
         </div>
         );
     }
